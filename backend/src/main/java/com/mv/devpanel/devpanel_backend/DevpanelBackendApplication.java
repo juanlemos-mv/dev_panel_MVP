fbd578c5c78084d001643682976fe68375a025f0
Juan Guilherme Silva Lemos <juan.lemos@mv.com.br>
2026-02-11 14:54:19 -0300

    chore(backend): set up Maven monorepo and align Spring Boot/Java

---
+++ backend/src/main/java/com/mv/devpanel/devpanel_backend/DevpanelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mv.devpanel.devpanel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpanelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpanelBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mv/devpanel/devpanel_backend/DevpanelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.devpanel.devpanel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpanelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
